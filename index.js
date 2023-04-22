14325c201a9c4a8d08e675c5a59d337eb9dd497e
Aditya Ghadge <adityaghadge360@gmail.com>
2023-04-23 04:25:11 +0530

    Create a  node server

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        fs.readFile('./index.html', 'UTF-8', (err, data) => {
+            if(err){
+                console.error(err);
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }else if(req.url == '/about'){
+        fs.readFile('./about.html', 'UTF-8', (err, data) => {
+            if(err){
+                console.error(err);
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }else if(req.url == '/contact-me'){
+        fs.readFile('./contact-me.html', 'UTF-8', (err, data) => {
+            if(err){
+                console.error(err);
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }else{
+        fs.readFile('./404.html', 'UTF-8', (err, data) => {
+            if(err){
+                console.error(err);
+            }
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
